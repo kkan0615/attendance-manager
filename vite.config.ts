bbe86a30d65820d66c940e935861c5fdc7971eb2
unknown <kkan0615@gmail.com>
2022-05-31 10:55:43 -0400

    :zap: Feat: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { fileURLToPath, URL } from 'url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +10,40 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls }
     }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      }
+    }),
+    // VitePWA({
+    //   mode: 'development',
+    //   base: '/',
+    //   // srcDir: 'src',
+    //   // filename: 'serviceWorker.ts',
+    //   // includeAssets: ['/favicon.png'],
+    //   // strategies: 'injectManifest',
+    //   // manifest: {
+    //   //   name: 'attendance-manager',
+    //   //   short_name: 'attendance',
+    //   //   start_url: './',
+    //   //   description: 'Manager your employers attendance easily!',
+    //   //   theme_color: '#FFA726',
+    //   //   display: 'standalone',
+    //   //   icons: [
+    //   //     {
+    //   //       src: './public/icons/logo_48x48.png',
+    //   //       sizes: '48x48',
+    //   //     },
+    //   //     {
+    //   //       src: './public/icons/logo_256x256.png',
+    //   //       sizes: '256x256'
+    //   //     },
+    //   //   ]
+    //   // },
+    // }),
     quasar({
       sassVariables: 'src/styles/libs/quasar/variables.sass'
-    })
+    }),
   ],
   server: {
     open: true,
